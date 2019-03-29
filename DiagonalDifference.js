b469e8acba99d4c15fee0e0fef33b2878057985f
hocineimail <hocineismail75@gmail.com>
2019-03-29 01:25:44 +0100

    Diagonal Difference

---
+++ DiagonalDifference.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the diagonalDifference function below.
+function diagonalDifference(arr) {
+    let a = 0;
+    let b = 0;
+    let j = arr.length - 1;
+    for (let i = 0; i < arr.length; i++) {
+
+        a = a + arr[i][i]
+        b = b + arr[i][j]
+        j = j -1
+    }
+    var solution
+    var result = a - b
+    if (result < 0) {
+          solution = result * -1
+    } else {
+        solution = result
+    }
+    return solution
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let arr = Array(n);
+
+    for (let i = 0; i < n; i++) {
+        arr[i] = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+
+    const result = diagonalDifference(arr);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
